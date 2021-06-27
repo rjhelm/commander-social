2467df4c3277220060436ddfd3f889323251e66b
Ryan Helm <rjhelm@github.io>
2021-06-27 01:02:45 -0600

    addded helpers started signin

--- server/constants/Subscriptions.js
+++ server/constants/Subscriptions.js
@@ -1,3 +1,55 @@
+import { createClient } from 'graphql-ws';
+
+const client = createClient({
+  url: 'ws://welcomer.com:4000/graphql',
+});
+
+// query
+(async () => {
+  const result = await new Promise((resolve, reject) => {
+    let result;
+    client.subscribe(
+      {
+        query: '{ hello }',
+      },
+      {
+        next: (data) => (result = data),
+        error: reject,
+        complete: () => resolve(result),
+      },
+    );
+  });
+
+  expect(result).toEqual({ hello: 'Hello World!' });
+})();
+
+// subscription
+(async () => {
+  const onNext = () => {
+    /* handle incoming values */
+  };
+
+  let unsubscribe = () => {
+    /* complete the subscription */
+  };
+
+  await new Promise((resolve, reject) => {
+    unsubscribe = client.subscribe(
+      {
+        query: 'subscription { greetings }',
+      },
+      {
+        next: onNext,
+        error: reject,
+        complete: resolve,
+      },
+    );
+  });
+
+  expect(onNext).toBeCalledTimes(5); // we say "Hi" in 5 languages
+})();
+
+
 export const MESSAGE_CREATED = 'MESSAGE_CREATED';
 
 export const IS_USER_ONLINE = 'IS_USER_ONLINE';
